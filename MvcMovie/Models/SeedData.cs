f8a4f7d6de2cb71bf302e5c0e1be6e205513dbaa
Drazen Lucic <lucicd@hotmail.com>
2019-03-14 08:31:55 +0400

    Change to LDS movies

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -22,38 +22,47 @@ namespace MvcMovie.Models
                 context.Movie.AddRange(
                     new Movie
                     {
-                        Title = "When Harry Met Sally",
-                        ReleaseDate = DateTime.Parse("1989-2-12"),
-                        Genre = "Romantic Comedy",
-                        Rating = "R",
-                        Price = 7.99M
-                    },
-
-                    new Movie
-                    {
-                        Title = "Ghostbusters",
-                        ReleaseDate = DateTime.Parse("1984-3-13"),
-                        Genre = "Comedy",
-                        Rating = "PG",
-                        Price = 8.99M
-                    },
-
-                    new Movie
-                    {
-                        Title = "Ghostbusters 2",
-                        ReleaseDate = DateTime.Parse("1986-2-23"),
-                        Genre = "Comedy",
-                        Rating = "PG",
+                        Title = "Best Two Years",
+                        ReleaseDate = DateTime.Parse("2004-2-20"),
+                        Genre = "Comedy, Drama",
+                        Rating = "PG-13",
                         Price = 9.99M
                     },
 
                     new Movie
                     {
-                        Title = "Rio Bravo",
-                        ReleaseDate = DateTime.Parse("1959-4-15"),
-                        Genre = "Western",
+                        Title = "Joseph Smith: Prophet of the Restoration",
+                        ReleaseDate = DateTime.Parse("2005-1-1"),
+                        Genre = "Biography, Drama, History",
                         Rating = "None",
-                        Price = 3.99M
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Other Side of Heaven",
+                        ReleaseDate = DateTime.Parse("2002-4-12"),
+                        Genre = "Adventure, Biography, Drama",
+                        Rating = "PG",
+                        Price = 14.98M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Trek: The Movie",
+                        ReleaseDate = DateTime.Parse("2008-4-6"),
+                        Genre = " Comedy, Drama, Family",
+                        Rating = "PG",
+                        Price = 19.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Come Unto Me",
+                        ReleaseDate = DateTime.Parse("2014-4-1"),
+                        Genre = "Short, Drama, Family",
+                        Rating = "None",
+                        Price = 4.99M
                     }
                 );
                 context.SaveChanges();
